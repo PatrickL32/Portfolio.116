715ed92f294fd9139a04c49fd9a2b7a421e4217b
PatrickL32 <Jedi.92.lewis@gmail.com>
2025-08-20 20:04:30 -0700

    second commit

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class skill(models.model):
+    name = models.CharField(max_length=100)
+
+    
+
+
+class project(models.model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    year = models.IntegerField()
+    image = models.ImageField(upload_to='prjects/')
+    skills = models.ManyToManyField('skill')
+    repository = models.URLField()
+
+
+    def __str__(self):
+        return f'{self.name} - {self.year}'
--- pages/views.py
+++ pages/views.py
@@ -1,9 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
-def homePageView(request):
-    return render(request, 'pages/home.html')
-
 def aboutPageView(request):
     return render(request, 'pages/about.html')
 
